11f68ef5ee548ccd9c8529e67eed04de66b70c00
williamtruppi <williamt@hotmail.it>
2021-02-23 15:14:02 +0100

    suddivisione in partials e layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,3 @@
+@include("layouts.head");
+@include("layouts.header");
+@include("layouts.footer");
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,13 @@
+<div class="flex-center position-ref">
+            <div class="content">
+                <div class="title m-b-md">
+                    Laravel
+                </div>
+
+                <div class="links">
+                    <a href="{{route("home")}}">Home</a>
+                    <a href="{{route("posts.index")}}">posts</a>
+                    <a href="{{route("about")}}">About</a>
+                </div>
+            </div>
+        </div>
