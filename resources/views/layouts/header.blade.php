561725748ea90b618f1af3f5253dd4275ad9eaec
williamtruppi <williamt@hotmail.it>
2021-02-27 19:08:40 +0100

    index.blade delle CATEGORIES correttamente implementato

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,6 +7,7 @@
                 <div class="links">
                     <a href="{{route('home')}}">Home</a>
                     <a href="{{route('posts.index')}}">posts</a>
+                    <a href="{{route('categories.index')}}">categories</a>
                     <a href="{{route('about')}}">About</a>
                 </div>
             </div>
